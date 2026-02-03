f5ddf50de2baa2e3109ee90f7ac350e266fee6b8
PalomaCardozo <palomacardozo88@gmail.com>
2026-02-03 18:51:10 +0100

    Implement complete card matching flow

--- script.js
+++ script.js
@@ -2,154 +2,163 @@ const availableCards = [
   {
     name: "frog1",
     image: "Images/frog1.png",
+    alt: "Smiling frog",
     color: "#ee76f8",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog2",
     image: "Images/frog2.png",
+    alt: "Frog playing guitar",
     color: "#3a93f9",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog3",
     image: "Images/frog3.png",
+    alt: "Frog with umbrella",
     color: "#f2ba49",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog4",
     image: "Images/frog4.png",
+    alt: "Frog putting on makeup",
     color: "#5dcd5d",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog5",
     image: "Images/frog5.png",
+    alt: "Dancing frog",
     color: "#db7866",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog6",
     image: "Images/frog6.png",
+    alt: "Frog with a microphone",
     color: "#9566db",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog7",
     image: "Images/frog7.png",
+    alt: "Frog with a walking stick",
     color: "#76f887",
-    flipped: false,
     matched: false,
   },
 
   {
     name: "frog8",
     image: "Images/frog8.png",
+    alt: "Frog eating",
     color: "#a476f8",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog9",
     image: "Images/frog9.png",
+    alt: "Frog waving",
     color: "#fc6c56",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog10",
     image: "Images/frog10.png",
+    alt: "Frog lying down",
     color: "#3d5fe3",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog11",
     image: "Images/frog11.png",
+    alt: "Frog swimming",
     color: "#f876a6",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog12",
     image: "Images/frog12.png",
+    alt: "Frog jumping",
     color: "#f2f876",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog13",
     image: "Images/frog13.png",
+    alt: "Frog drinking a martini",
     color: "#768cf8",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog14",
     image: "Images/frog14.png",
+    alt: "Singing frog",
     color: "#942e9e",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog15",
     image: "Images/frog15.png",
+    alt: "Happy frog",
     color: "#3fe1f3",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog16",
     image: "Images/frog16.png",
+    alt: "Frog saying goodbye",
     color: "#f87676",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog17",
     image: "Images/frog17.png",
+    alt: "Frog sunbathing",
     color: "#f8e276",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog18",
     image: "Images/frog18.png",
+    alt: "Frog catching a fish",
     color: "#8afd7b",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog19",
     image: "Images/frog19.png",
+    alt: "Frog smelling a flower",
     color: "#43bef7",
-    flipped: false,
     matched: false,
   },
   {
     name: "frog20",
     image: "Images/frog20.png",
+    alt: "Frog playing a harmonica",
     color: "#fe67ae",
-    flipped: false,
     matched: false,
   },
 ];
 
-let selectedCards = [];
+const cardFrontImageSrc = "Images/lotus-flower.png";
+
+const gameBoard = document.querySelector(".container");
 
 let gameCards = [];
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard = null;
+let secondCard = null;
 
 function startGame(numberOfPairs) {
-  selectedCards = availableCards.slice(0, numberOfPairs);
-  gameCards = [...selectedCards, ...selectedCards];
-  gameCards = shuffleArray(gameCards);
+  gameBoard.innerHTML = "";
+
+  const selectedCards = availableCards.slice(0, numberOfPairs);
+  gameCards = shuffleArray([...selectedCards, ...selectedCards]);
+
+  renderGameBoard();
 }
 
 function shuffleArray(array) {
@@ -157,67 +166,59 @@ function shuffleArray(array) {
 }
 
 function renderGameBoard() {
-  const gameBoard = document.createElement("div");
-  gameBoard.className = "container";
-
   gameCards.forEach((card) => {
     const cardElement = document.createElement("div");
     cardElement.className = "flip-card";
 
     const cardInner = document.createElement("div");
     cardInner.className = "flip-card-inner";
-    cardElement.dataset.name = card.name;
-    cardElement.dataset.matched = card.matched;
+    cardInner.dataset.name = card.name;
 
     const cardFront = document.createElement("div");
     cardFront.className = "flip-card-front";
+
+    const cardFrontImage = document.createElement("img");
+    cardFrontImage.src = cardFrontImageSrc;
+    cardFrontImage.alt = "Lotus Flower";
 
     const cardBack = document.createElement("div");
     cardBack.className = "flip-card-back";
     cardBack.style.backgroundColor = card.color;
 
-    const cardImage = document.createElement("img");
-    cardImage.src = card.image;
+    const cardBackImage = document.createElement("img");
+    cardBackImage.src = card.image;
+    cardBackImage.alt = card.alt;
 
-    cardBack.appendChild(cardImage);
+    cardFront.appendChild(cardFrontImage);
+    cardBack.appendChild(cardBackImage);
     cardInner.appendChild(cardFront);
     cardInner.appendChild(cardBack);
     cardElement.appendChild(cardInner);
     gameBoard.appendChild(cardElement);
+
+    cardInner.addEventListener("click", flipCard);
   });
-
-  document.body.appendChild(gameBoard);
 }
-
-startGame(6);
-renderGameBoard();
-
-/*
-const flippedCard = document.querySelectorAll(".flip-card-inner");
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
 
 function flipCard() {
   if (lockBoard) return;
+  if (this === firstCard) return;
 
-  const card = this;
-  card.classList.add("flipped");
-
-  if (card === firstCard) return;
+  this.classList.add("flipped");
 
   if (!hasFlippedCard) {
     hasFlippedCard = true;
-    firstCard = card;
+    firstCard = this;
     return;
   }
 
-  secondCard = card;
+  secondCard = this;
   checkForMatch();
 }
 
 function checkForMatch() {
-  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+  const isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
   if (isMatch) {
     disableCards();
   } else {
@@ -246,8 +247,4 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
 }
 
-
-shuffle();
-
-flippedCard.forEach((card) => card.addEventListener("click", flipCard));
-*/
+startGame(6);
